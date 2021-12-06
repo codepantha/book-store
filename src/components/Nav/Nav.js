42435e4e4deb23b62f9c82f2b1c2a815d27afec4
codepanther <pezechigozirim@gmail.com>
2021-12-06 23:54:24 +0100

    Style the navbar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ImUser } from 'react-icons/im';
 import './Nav.css';
 
 const Nav = () => (
-  <nav>
-    <p>
-      <Link to="/books">Books</Link>
-    </p>
-    <p>
-      <Link to="/categories">Categories</Link>
-    </p>
+  <nav className="nav__container">
+    <p className="nav__logo">BookStore</p>
+    <ul className="nav__container-items">
+      <li className="nav__container-items_link">
+        <Link to="/books" className="link">Books</Link>
+      </li>
+      <li className="nav__container-items_link">
+        <Link to="/categories" className="link">Categories</Link>
+      </li>
+    </ul>
+    <ImUser />
   </nav>
 );
 
